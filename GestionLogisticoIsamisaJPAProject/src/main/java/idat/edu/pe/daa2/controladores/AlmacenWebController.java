2682f44f3f85fb9516f828af08d0c5737c349a1e
Brayan Madrid <Brayan Madrid@Brayan-PC>
2020-01-02 21:55:16 -0500

    ACTUALIZAR

---
+++ GestionLogisticoIsamisaJPAProject/src/main/java/idat/edu/pe/daa2/controladores/AlmacenWebController.java
@@ -0,0 +1,53 @@
+package idat.edu.pe.daa2.controladores;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.servlet.ModelAndView;
+
+import idat.edu.pe.daa2.jpa.modelo.Almacen;
+import idat.edu.pe.daa2.jpa.servicios.AlmacenServicio;
+
+@Controller
+@RequestMapping("/almacen")
+public class AlmacenWebController {
+	
+	@Autowired
+	private AlmacenServicio servicioAlmacen;
+	
+	@RequestMapping("/nuevo")
+	public String nuevaAlmacen(Model model) {
+		Almacen almacen = new Almacen();
+		model.addAttribute("almacen", almacen);
+		return "/moduloAdministrativo/nuevaAlmacen";
+	}
+	 
+	
+	@RequestMapping(value = "/guardar", method = RequestMethod.POST)
+	public String crearAlmacen(@ModelAttribute("almacen") Almacen almacen) {
+		servicioAlmacen.crear(almacen);
+	    return "redirect:/proptrans/listarTodo";
+	}
+	
+	@RequestMapping("/actualizar/{id}")
+	public ModelAndView editarAlmacen(@PathVariable(name = "id") int id) {
+	    ModelAndView mav = new ModelAndView("/moduloAdministrativo/editarAlmacen");
+	    Almacen almacen = servicioAlmacen.buscarPorID(id);
+	    mav.addObject("almacen", almacen);
+	     
+	    return mav;
+	}
+	
+	
+	@RequestMapping("/eliminar/{id}")
+	public String eliminarAlmacen
+	(@PathVariable(name = "id") int id) {
+		servicioAlmacen.borrarPorID(id);;
+	    return "redirect:/proptrans/listarTodo";       
+	}
+
+}
---
+++ GestionLogisticoIsamisaJPAProject/src/main/java/idat/edu/pe/daa2/controladores/SubAlmacenWebController.java
@@ -0,0 +1,53 @@
+package idat.edu.pe.daa2.controladores;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.servlet.ModelAndView;
+
+import idat.edu.pe.daa2.jpa.modelo.SubAlmacen;
+import idat.edu.pe.daa2.jpa.servicios.SubAlmacenServicio;
+
+@Controller
+@RequestMapping("/subalmacen")
+public class SubAlmacenWebController {
+	
+	@Autowired
+	private SubAlmacenServicio servicioSubAlmacen;
+	
+	@RequestMapping("/nuevo")
+	public String nuevaSubAlmacen(Model model) {
+		SubAlmacen subalmacen = new SubAlmacen();
+		model.addAttribute("subalmacen", subalmacen);
+		return "/moduloAdministrativo/nuevaSubAlmacen";
+	}
+	 
+	
+	@RequestMapping(value = "/guardar", method = RequestMethod.POST)
+	public String crearSubAlmacen(@ModelAttribute("subalmacen") SubAlmacen subalmacen) {
+		servicioSubAlmacen.crear(subalmacen);
+	    return "redirect:/proptrans/listarTodo";
+	}
+	
+	@RequestMapping("/actualizar/{id}")
+	public ModelAndView editarSubAlmacen(@PathVariable(name = "id") int id) {
+	    ModelAndView mav = new ModelAndView("/moduloAdministrativo/editarSubAlmacen");
+	    SubAlmacen subalmacen = servicioSubAlmacen.buscarPorID(id);
+	    mav.addObject("subalmacen", subalmacen);
+	     
+	    return mav;
+	}
+	
+	
+	@RequestMapping("/eliminar/{id}")
+	public String eliminarSubAlmacen
+	(@PathVariable(name = "id") int id) {
+		servicioSubAlmacen.borrarPorID(id);;
+	    return "redirect:/proptrans/listarTodo";       
+	}
+
+}
